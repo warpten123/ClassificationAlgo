99a0cf1a1451856750a97e02bb5835b66ea1a5ad
Paul Joshua Premacio <paul.premacio.20@usjr.edu.ph>
2022-08-18 19:03:53 +0800

    formatted the code

--- text_processing.py
+++ text_processing.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 from multiprocessing.resource_sharer import stop
 import docx2txt #lib for reading docx files
 
@@ -5,38 +6,45 @@ texts_from_file = docx2txt.process("Introduction.docx")
 print(texts_from_file)
 
 
-text = []
+def text_tokenization(text):
+    text = texts_from_file.split()
+    return text
+
+def toLowerCase(text):
+    text = [word.lower() for word in text]
+    return text
+    
+def removeStopWords(text):
+    with open('stopwords.txt', 'r') as f:
+        stop_words = f.read().splitlines()
+    text = [word for word in text if word not in stop_words]
+    return text
+
+text,processedText = [],[]
 text = texts_from_file
+
+
 print("Initial Count of Words in File: " + str(len(text)))
 
+processedText = text_tokenization(text)
+processedText = toLowerCase(processedText)
+processedText = removeStopWords(processedText)
+print(processedText)
 # tokenize texts_from_file
-text = texts_from_file.split()
-print(text)
-print("Count of Words in File: " + str(len(text)))
+
 
 # lowercase the content from text
-text = [word.lower() for word in text]
-print(text)
-
-print(str(len(text)))
 
 
-# list of stop words
-# read the file in stop_words
-with open('stopwords.txt', 'r') as f:
-    stop_words = f.read().splitlines()
-print(stop_words)
-print(str(len(stop_words)))
 
-# remove stop words from text
-text = [word for word in text if word not in stop_words]
-print(text)
-print(str(len(text)))
+
+
+
 
 # check for repeating words in text and save it as a map<word,count>
-text = dict([(word, text.count(word)) for word in set(text)])
-print(text)
-print(str(len(text)))
+# text = dict([(word, text.count(word)) for word in set(text)])
+# print(text)
+# print(str(len(text)))
     
 
 
