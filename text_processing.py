743302321fbb001f9ebf2620e4e711b3ed8a7d89
Cristopher Bohol <cris.bacus6@gmail.com>
2022-08-18 18:43:08 +0800

    exp

--- text_processing.py
+++ text_processing.py
@@ -1,3 +1,4 @@
+from multiprocessing.resource_sharer import stop
 import docx2txt #lib for reading docx files
 
 texts_from_file = docx2txt.process("Introduction.docx")
@@ -7,4 +8,38 @@ print(texts_from_file)
 text = []
 text = texts_from_file
 print("Initial Count of Words in File: " + str(len(text)))
+
+# tokenize texts_from_file
+text = texts_from_file.split()
+print(text)
+print("Count of Words in File: " + str(len(text)))
+
+# lowercase the content from text
+text = [word.lower() for word in text]
+print(text)
+
+print(str(len(text)))
+
+
+# list of stop words
+# read the file in stop_words
+with open('stopwords.txt', 'r') as f:
+    stop_words = f.read().splitlines()
+print(stop_words)
+print(str(len(stop_words)))
+
+# remove stop words from text
+text = [word for word in text if word not in stop_words]
+print(text)
+print(str(len(text)))
+
+# check for repeating words in text and save it as a map<word,count>
+text = dict([(word, text.count(word)) for word in set(text)])
+print(text)
+print(str(len(text)))
+    
+
+
+
+
 
