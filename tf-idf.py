bb5f2776b33a614c64cf6bddc5c3218344bed6d8
Paul Joshua Premacio <paul.premacio.20@usjr.edu.ph>
2022-12-02 02:00:07 +0800

    new baseline

--- tf-idf.py
+++ tf-idf.py
@@ -9,6 +9,7 @@ from nltk.stem import PorterStemmer
 import os
 import math
 import numpy as np
+from fpdf import FPDF
 # class word:
 #     def __init__(self, word, count):
 #         self.word = word
@@ -21,7 +22,8 @@ class Processing():
 
     def getFromPDF(self):
         finalText = " "
-        with pdfplumber.open(r'../Data Set/Goal 2/goal 2_3.pdf') as pdf:
+        # with pdfplumber.open(r'../Data Set/Goal 1/goal 1_3.pdf') as pdf:
+        with pdfplumber.open(r'Goal 1 Training Set.pdf') as pdf:
             for page in pdf.pages:
                 extractFromPDF = page.extract_text()
                 finalText = finalText + extractFromPDF
@@ -29,7 +31,7 @@ class Processing():
         return finalText
 
     def toCSV(self, goals, index, dict):
-        filename = index + ".csv"
+        filename = str(index) + ".csv"
         direc = 'Term/' + goals + "/"
         with open(direc + filename, 'w') as csvFile:
             w = csv.DictWriter(csvFile, dict.keys())
@@ -37,19 +39,29 @@ class Processing():
             w.writerow(dict)
 
     def fromPDFFolders(self, goalName):
+        tf_idf = {}
         count = 0
         directory = (glob.glob("../Data Set/" + goalName + "/*.pdf"))
+        extractedText = " "
+        finalText = " "
         for file in directory:
             with pdfplumber.open(file) as pdf:
                 count += 1
-                # print("Count PDF #: " + str(count))
-        return count
+                print("Count PDF #: " + str(count))
+                for page in pdf.pages:
+                    extractedText = page.extract_text()
+            finalText = finalText + extractedText
+            print("(Extracted Text): " +
+                  str(count) + str(len(finalText)))
+            extractedText = ""
+            tf_idf = self.mainProcessing(finalText, goal, count)
+        return tf_idf
 
     def preProcessing(self, text):
         text = preProc.removeSpecialCharacters(text)
         text = preProc.manual_tokenization(text)
         text = preProc.removeStopWords(text)
-
+        # text = preProc.removeNumbers(text)
         # for t in text:
         #     finalText.append(preProc.removeNumbers(t))
         return text
@@ -101,7 +113,7 @@ class Processing():
 
     def countNumberofDocs(self):
         count = 0
-        dir_path = r'Term\Goal 2'
+        dir_path = r"Term/Goal 1"
         for path in os.listdir(dir_path):
             if os.path.isfile(os.path.join(dir_path, path)):
                 count += 1
@@ -113,7 +125,7 @@ class Processing():
 
     def csvToDict(self, number):
         diction = {}
-        filename = "Term/Goal 1/" + str(number) + ".csv"
+        filename = "Term/Goal 2/" + str(number) + ".csv"
         # df = pd.read_csv(filename)
         # for index, rows in df.iterrows():
         #     d = rows.to_dict()
@@ -126,28 +138,88 @@ class Processing():
     def convertingToDP(self, featureSet, tf_idf):
         df = pd.DataFrame.from_dict(tf_idf)
         df2 = df.replace(np.nan, 0)
-        df2.to_csv('file_name.csv')
+        df2.to_csv('Goal 2 TF IDF.csv')
         return df2
+
+    def mainProcessing(self, preProcesssedText, goal, index):
+        emptyDict = {}
+        term_freq = {}
+        finalFeatures = {}
+        dict1 = {}
+        dict2 = {}
+        dict3 = {}
+        idf = {}
+        tf_idf = {}
+        tf_idf2 = {}
+        tf_idf3 = {}
+        passToPD = [{}]
+        preprocessedText = TFIDF.preProcessing(processedText)
+        emptyDict = TFIDF.populateClass(preprocessedText)
+        term_freq = TFIDF.term_vectors(preprocessedText, emptyDict)
+        term_freq = TFIDF.term_frequency(term_freq)
+        TFIDF.toCSV(goal, str(index), term_freq)
+        dict1 = TFIDF.csvToDict(index)
+        # if index == 3:
+    ##### NEW TFIDF###
+
+    def extractAllPDF(self, goal):
+        count = 0
+        directory = (glob.glob("../Data Set/" + goal + "/*.pdf"))
+        extractedText = " "
+        finalText = " "
+        for file in directory:
+            with pdfplumber.open(file) as pdf:
+                count += 1
+                print("Count PDF #: " + str(count))
+                for page in pdf.pages:
+                    extractedText = page.extract_text()
+                    print("length: " + str(len(extractedText)))
+                    finalText = finalText + extractedText
+            # print("(Extracted Text): " +
+            #       str(count) + str(len(finalText)))
+
+        # tf_idf = self.mainProcessing(finalText, goal, count)
+        return finalText
+
+    def listToPDF(self, processedText, goal):
+        with open(r"Text Files/" + goal + ".txt", 'w') as fp:
+            fp.write(' '.join(processedText))
+        f = open("Text Files/" + goal + ".txt", "r")
+        pdf = FPDF()
+        pdf.set_font('Times', '', 12)
+        pdf.add_page()
+        for x in f:
+            pdf.cell(200, 10, txt=x, ln=1, align='C')
+        pdf.output("Training Set/" + goal + " Training Set" + ".pdf")
+    ### END NEW TF IDF###
 
 
 if __name__ == '__main__':
     goals = ['Goal 1', 'Goal 2']
-    emptyDict = {}
-    term_freq = {}
-    finalFeatures = {}
-    dict1 = {}
-    dict2 = {}
-    dict3 = {}
-    idf = {}
-    tf_idf = {}
-    tf_idf2 = {}
-    tf_idf3 = {}
-    passToPD = [{}]
-    processedText = ""
-    processedText = Processing(processedText)
-    TFIDF = Processing(processedText)
+    # for goal in goals:
+    #     processedText = ""
+    #     processedText = Processing(processedText)
+    #     TFIDF = Processing(processedText)
+    #     processedText = TFIDF.fromPDFFolders(goal)
+    rawText = ""
+    test = ""
+    TFIDF = Processing(rawText)
     # preprocessedText = TFIDF.getFromPDF()
+    for goal in goals:
+        rawText = TFIDF.extractAllPDF(goal)
+        print(goal + "=> Before Processing: " + str(len(rawText)))
+        preprocessedText = TFIDF.preProcessing(rawText)
+        print(goal + "=> After Processing: " + str(len(preprocessedText)))
+        TFIDF.listToPDF(preprocessedText, goal)
+    # print(preprocessedText)
+    # test = TFIDF.getFromPDF()
+    # test = list(test.split(" "))
+    # print(test)
 
+    # test = TFIDF.getFromPDF()
+    # print("FROM TEST: " + str(len(test)))
+
+    # print(len(processedText))
     # print("Before Processing: " + str(len(preprocessedText)))
     # preprocessedText = TFIDF.preProcessing(preprocessedText)
     # print("After Processing: " + str(len(preprocessedText)))
@@ -156,21 +228,22 @@ if __name__ == '__main__':
     # term_freq = TFIDF.term_frequency(emptyDict)
     # TFIDF.toCSV("Goal 2", str(3), term_freq)
     # print("CSV File Created!")
-    dict1 = TFIDF.csvToDict(1)
-    dict2 = TFIDF.csvToDict(2)
-    dict3 = TFIDF.csvToDict(3)
 
-    finalFeatures = TFIDF.mergeDictionaries(dict1, dict2, dict3)
-    # print("DICT 1: " + str(len(dict1)) + "DICT 2: " + str(len(dict2)))
-    for v in finalFeatures:
-        finalFeatures[v] = 0.0
-    idf = TFIDF.inverse_frequency(finalFeatures, dict1, dict2, dict3)
+    # dict1 = TFIDF.csvToDict(1)
+    # dict2 = TFIDF.csvToDict(2)
+    # dict3 = TFIDF.csvToDict(3)
 
-    tf_idf = TFIDF.computeTF_IDF(dict1, idf)
-    tf_idf2 = TFIDF.computeTF_IDF(dict2, idf)
-    tf_idf3 = TFIDF.computeTF_IDF(dict3, idf)
+    # finalFeatures = TFIDF.mergeDictionaries(dict1, dict2, dict3)
+    # # print("DICT 1: " + str(len(dict1)) + "DICT 2: " + str(len(dict2)))
+    # for v in finalFeatures:
+    #     finalFeatures[v] = 0.0
+    # idf = TFIDF.inverse_frequency(finalFeatures, dict1, dict2, dict3)
 
-    passToPD = tf_idf, tf_idf2, tf_idf3
-    # tfidf.toCSV("Goal 1", "TFIDF", tf_idf)
-    tf_idf = TFIDF.convertingToDP(finalFeatures, passToPD)
-    print(tf_idf)
+    # tf_idf = TFIDF.computeTF_IDF(dict1, idf)
+    # tf_idf2 = TFIDF.computeTF_IDF(dict2, idf)
+    # tf_idf3 = TFIDF.computeTF_IDF(dict3, idf)
+
+    # passToPD = tf_idf, tf_idf2, tf_idf3
+    # # tfidf.toCSV("Goal 1", "TFIDF", tf_idf)
+    # tf_idf = TFIDF.convertingToDP(finalFeatures, passToPD)
+    # print(tf_idf)
